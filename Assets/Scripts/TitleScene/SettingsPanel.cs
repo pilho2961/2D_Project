7cb17cb600868a2a4c105f724f151b65979b83ff
pilho2961 <peterlee831@gmail.com>
2024-01-18 13:19:47 +0900

    240118day

--- Assets/Scripts/TitleScene/SettingsPanel.cs
+++ Assets/Scripts/TitleScene/SettingsPanel.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingsPanel : MonoBehaviour
 {
     Button closeButton;
-    public float maxBGMSound;
-    public float maxEffectSound;
-    public float currentBGM;
-    public float currentEffectSound;
+    public AudioMixer mixer;
+
+    // º¼·ý Á¶Àý
+    public void SetLevel(float sliderVal)
+    {
+        mixer.SetFloat("MasterVolume", Mathf.Log10(sliderVal) * 20);
+    }
 
     private void Awake()
     {
